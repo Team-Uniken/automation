275c4d673384eb1742de0541f2d8b69188eefc80
husain <hussain.kantawala@uniken.com>
2016-10-24 18:31:27 +0530

    Facebook sdk integration done

--- REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
+++ REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
@@ -4,10 +4,13 @@ import android.app.Application;
 import android.util.Log;
 
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +19,19 @@ import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import io.neson.react.notification.NotificationPackage;
 
 public class MainApplication extends Application implements ReactApplication {
+
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -30,7 +46,8 @@ public class MainApplication extends Application implements ReactApplication {
               new RDNAReactPackage(),
               new ExtraDimensionsPackage(MainActivity.currentActivity),
               new ReactNativePushNotificationPackage(),
-              new NotificationPackage(MainActivity.currentActivity)
+              new NotificationPackage(MainActivity.currentActivity),
+              new FBSDKPackage(getCallbackManager())
       );
     }
   };
