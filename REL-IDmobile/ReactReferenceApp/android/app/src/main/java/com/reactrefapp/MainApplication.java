ade36e340567477616800d4f538ec101b4c996a4
husain <hussain.kantawala@uniken.com>
2016-11-04 16:16:16 +0530

    key to find facebook keyhash added in comments

--- REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
+++ REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
@@ -1,6 +1,10 @@
 package com.reactrefapp;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
 import android.util.Log;
 
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -13,6 +17,8 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,6 +37,21 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         FacebookSdk.sdkInitialize(getApplicationContext());
+        //Code for facebook key hash
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.reactrefapp",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
     }
 
 
