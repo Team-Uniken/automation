30d68da9dc13cdde00395c7e2834efc3bed335c5
nikhiljaiswal146 <nikhil.jaiswal@uniken.com>
2016-12-01 13:34:01 +0530

    resolve conflict

--- REL-IDmobile/ReactReferenceApp/App/Scenes/Load.js
+++ REL-IDmobile/ReactReferenceApp/App/Scenes/Load.js
@@ -42,21 +42,11 @@ let appalive = false;
 
 console.log = function () { }
 
-<<<<<<< HEAD
-const {Text, View, Animated, InteractionManager, AppState, Image, AsyncStorage, Alert, Platform, BackAndroid, StatusBar,
-=======
-const {StatusBar, Text, View, Animated, InteractionManager, AppState, Image, AsyncStorage, Alert, Platform, BackAndroid,
->>>>>>> 9145bd244d8da5d6458c1bc1ccff1b75cf317c39
-  //Push notification code
-  PushNotificationIOS, AppStateIOS, AlertIOS, StyleSheet, } = ReactNative;
+const {Text, View, Animated, InteractionManager, AppState, Image, AsyncStorage, Alert, Platform, BackAndroid, StatusBar,PushNotificationIOS, AppStateIOS, AlertIOS, StyleSheet, } = ReactNative;
 
 const {Component} = React;
 
-<<<<<<< HEAD
 BackAndroid.addEventListener('hardwareBackPress', function () {
-=======
-BackAndroid.addEventListener('hardwareBackPress', function() {
->>>>>>> 9145bd244d8da5d6458c1bc1ccff1b75cf317c39
   return true;
 });
 
@@ -430,7 +420,6 @@ class Load extends Component {
 
   }
   _handleAppStateChange(currentAppState) {
-<<<<<<< HEAD
     //    console.log('_handleAppStateChange');
     //    console.log(currentAppState);
     //
@@ -463,40 +452,6 @@ class Load extends Component {
     //    } else if (currentAppState === 'inactive') {
     //      console.log('App State Change Inactive');
     //    }
-=======
-    console.log('_handleAppStateChange');
-    console.log(currentAppState);
-
-    if (currentAppState == 'background') {
-      console.log('App State Change background:');
-
-      ReactRdna.pauseRuntime((response) => {
-        if (response) {
-          if (response[0].error == 0) {
-            AsyncStorage.setItem("savedContext", response[0].response);
-          }
-          console.log('Immediate response is ' + response[0].error);
-        } else {
-          console.log('No response.');
-        }
-      })
-    } else if (currentAppState == 'active') {
-      console.log('App State Change active:');
-      AsyncStorage.getItem("savedContext").then((value) => {
-        if (value != null) {
-          ReactRdna.resumeRuntime(value, null, (response) => {
-            if (response) {
-              console.log('Immediate response is ' + response[0].error);
-            } else {
-              console.log('No response.');
-            }
-          })
-        }
-      }).done();
-    } else if (currentAppState === 'inactive') {
-      console.log('App State Change Inactive');
-    }
->>>>>>> 9145bd244d8da5d6458c1bc1ccff1b75cf317c39
   }
 
 
