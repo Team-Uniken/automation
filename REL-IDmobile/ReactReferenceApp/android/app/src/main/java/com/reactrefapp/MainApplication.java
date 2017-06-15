b456673c9074524a65957a02362b41243dcbbe3b
hussainkantawala <hussain.kantawala@uniken.com>
2017-06-15 14:58:54 +0530

    Unused packages removed and push notification shown in foreground

--- REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
+++ REL-IDmobile/ReactReferenceApp/android/app/src/main/java/com/reactrefapp/MainApplication.java
@@ -1,25 +1,14 @@
 package com.reactrefapp;
 
 import android.app.Application;
-import android.app.NotificationManager;
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.multidex.MultiDex;
-import android.util.Base64;
 
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.dieam.reactnativepushnotification.modules.RNPushNotificationHelper;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
-import com.burnweb.rnwebview.RNWebViewPackage;
-import io.neson.react.notification.NotificationPackage;
-import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
+
 import com.airbnb.android.react.maps.MapsPackage;
-import com.oney.gcm.GcmPackage;
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,13 +16,9 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 
-//import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
-//import io.neson.react.notification.NotificationPackage;
 import com.facebook.react.modules.network.ReactCookieJarContainer;
 import com.facebook.stetho.Stetho;
 import okhttp3.OkHttpClient;
@@ -62,19 +47,19 @@ public class MainApplication extends Application implements ReactApplication {
         super.onCreate();
 
 
-      //  if(BuildConfig.DEBUG) {
-            Stetho.initializeWithDefaults(this);
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .connectTimeout(0, TimeUnit.MILLISECONDS)
-                    .readTimeout(0, TimeUnit.MILLISECONDS)
-                    .writeTimeout(0, TimeUnit.MILLISECONDS)
-                    .cookieJar(new ReactCookieJarContainer())
-                    .addNetworkInterceptor(new StethoInterceptor())
-                    .build();
-            OkHttpClientProvider.replaceOkHttpClient(client);
-       // }
+        //  if(BuildConfig.DEBUG) {
+        Stetho.initializeWithDefaults(this);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(0, TimeUnit.MILLISECONDS)
+                .readTimeout(0, TimeUnit.MILLISECONDS)
+                .writeTimeout(0, TimeUnit.MILLISECONDS)
+                .cookieJar(new ReactCookieJarContainer())
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        OkHttpClientProvider.replaceOkHttpClient(client);
+        // }
         //FacebookSdk.sdkInitialize(getApplicationContext());
-      //  Code for facebook key hash
+        //  Code for facebook key hash
 //        try {
 //            PackageInfo info = getPackageManager().getPackageInfo(BuildConfig.APPLICATION_ID,
 //                    PackageManager.GET_SIGNATURES);
@@ -91,34 +76,28 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new MapsPackage(),
+                    new ReactNativeConfigPackage(),
+                    new RDNAReactPackage(),
+                    new ReactNativePushNotificationPackage(),
+                    new FBSDKPackage(getCallbackManager()),
+                    new RCTCameraPackage()
+            );
+        }
+    };
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-              new MainReactPackage(),
-            new RNWebViewPackage(),
-          //  new NotificationPackage(),
-            new ReactNativePermissionsPackage(),
-            new MapsPackage(),
-            new GcmPackage(),
-           // new ExtraDimensionsPackage(),
-            new ReactNativeConfigPackage(),
-              new RDNAReactPackage(),
-             // new ExtraDimensionsPackage(MainActivity.currentActivity),
-              new ReactNativePushNotificationPackage(),
-              new FBSDKPackage(getCallbackManager()),
-              new RCTCameraPackage()
-      );
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
 }
