db8165025463bbe7a58d50c3f5cfd500a6dc61f6
swapnil gavali <swapnil.gavali@uniken.com>
2018-10-04 12:53:58 +0530

    -dashboard folder added

---
+++ REL-IDmobile/ReactReferenceApp/App/Components/Dashboard/ChathamRetail/Accounts.js
@@ -0,0 +1,411 @@
+'use strict';
+/*
+    NEED
+ */
+
+import React, { Component, PropTypes } from 'react';
+import ReactNative, { View, Text, StyleSheet,ListView, Image ,Platform,TouchableOpacity} from 'react-native'
+import Events from 'react-native-simple-events';
+//import { FormattedCurrency } from 'react-native-globalize';
+import Skin from '../../../Skin';
+import Main from '../../Container/Main';
+import ListItem from '../../ListItem';
+import ContactScene from './Contact';
+import PayBillsScene from './PayBills';
+import DepositsScene from './Deposits';
+import FindBranchScene from './FindBranch';
+import { SearchBar } from 'react-native-elements'
+
+/*
+  CALLED
+*/
+import ListSectionHeader from '../../ListSectionHeader';
+import { FormattedCurrency } from 'react-native-globalize';
+var ReactRdna = require('react-native').NativeModules.ReactRdnaModule;
+
+var titleStringArray = ["ACCOUNTS","PAY BILLS","DEPOSITS","FIND BRANCH","CONTACT"];
+/*
+  Instantiaions
+*/
+// const ReactRdna = React.NativeModules.ReactRdnaModule;
+const RDNARequestUtility = ReactNative.NativeModules.RDNARequestUtility;
+
+import ControlPanel from '../ControlPanel';
+import Config from 'react-native-config';
+import NavBar from '../../view/navbar.js';
+import PageTitle from '../../view/pagetitle.js';
+
+var sud =0;
+let self;
+const headers = {
+  1: 'Savings',
+  2: 'Investments',
+  3: 'Credit Cards',
+};
+const icons = { 1: '\ue2f7',2: '\ue277',3: '\ue285', };
+
+const iconcolor = {
+  1: Skin.colors.DIVIDER_COLOR,
+  2: Skin.colors.POSITIVE_ACCENT,
+  3: Skin.colors.ACCENT,
+};
+
+
+export default class AccountsScene extends Component {
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
+    this.state = { dataSource: ds.cloneWithRowsAndSections([]), tabChanged:1,activeTab :1,titleString:"Accounts"};
+    self = this;
+    this.tabChanged = this.tabChanged.bind(this);
+  }
+
+  /**
+   * Creates the self object for reference later
+   * @return {null}
+   */
+  componentDidMount() {
+    this.getMyNotifications();
+    this.getAccountDetails();
+      Events.on('tabChanged', 'tabChanged', this.tabChanged);
+  }
+
+
+  getMyNotifications() {
+
+    var recordCount = "0";
+    var startIndex = "1";
+    var enterpriseID = "";
+    var startDate = "";
+    var endDate = "";
+    ReactRdna.getNotifications(recordCount, startIndex, enterpriseID, startDate, endDate, (response) => {
+
+      console.log('----- NotificationMgmt.getMyNotifications.response ');
+      console.log(response);
+
+      if (response[0].error !== 0) {
+        console.log('----- ----- response is not 0');
+      //                               if (NotificationObtianedResponse !== undefined) {
+      //                               // If error occurred reload last response
+      //
+      //                                                              }
+      }
+
+    });
+  }
+
+
+
+  /**
+   * Retrieves the account data in a json format
+   * @return {[type]} [description]
+   */
+  getAccountDetails() {
+    //    RDNARequestUtility.doHTTPGetRequest('http://apisdkdev.uniken.com:8080/APIBanking/getAccounts.jsp?userid=10',
+    //      (response) => {
+    /*
+        REAL DATA
+     "{
+       "accountList":[
+        {"accountID":44,"accountName":"CANDEMOACT10_01","accountBalance":5565,"accountType":1},
+        {"accountID":45,"accountName":"CANDEMOACT10_02","accountBalance":24322,"accountType":1},
+        {"accountID":46,"accountName":"CANDEMOACT10_03","accountBalance":30039,"accountType":1},
+        {"accountID":47,"accountName":"CANDEMOACT10_04","accountBalance":40074,"accountType":1}
+       ],
+       "error":"",
+       "status":"success"
+      }
+    */
+    var response = {
+      0: {
+        error: 0,
+        response: '{"accountList":[ \
+                  {"accountID":"$35,233","accountName":"CANDEMOACT10_01","nickname":"Personal Savings","accountBalance":"$93,323.00","accountType":1}, \
+                  {"accountID":"$93,323","accountName":"CANDEMOACT10_03","nickname":"Equities","accountBalance":20643.00,"accountType":2}, \
+                  {"accountID":"$14,199","accountName":"CANDEMOACT10_04","nickname":"Platinum Card","accountBalance":1452111,"accountType":3}, \
+                  {"accountID":"$1,452,111","accountName":"Funds","nickname":"","accountBalance":"$93,323","accountType":2}, \
+                  {"accountID":"$20,643","accountName":"CANDEMOACT10_03","nickname":"Bonds","accountBalance":0,"accountType":2}, \
+                  {"accountID":"$9,912","accountName":"CANDEMOACT10_04","nickname":"Gold Card","accountBalance":117987,"accountType":3}], \
+              "error":"", \
+              "status":"success" \
+            }',
+      },
+    };
+    if (response[0].error === 0) {
+      const responseJson = JSON.parse(response[0].response);
+      if (responseJson.status === 'success') {
+        const accts = responseJson.accountList;
+        const {data, sectionIds} = self.renderListViewData(accts.sort(self.compare));
+        self.setState({
+          dataSource: self.state.dataSource.cloneWithRowsAndSections(data, sectionIds),
+        });
+        return { data };
+      } else {
+        alert(responseJson.status);
+      }
+    } else {
+      alert(response[0].response);
+    }
+  //      }
+  //    );
+  }
+
+  /**
+   * Basic sorting function for map calls
+   */
+  compare(a, b) {
+    if (a.accountName < b.accountName) {
+      return -1;
+    }
+    if (a.accountName > b.accountName) {
+      return 1;
+    }
+    return 0;
+  }
+
+  cleanRowData(rowData) {
+    const cleanData = {
+      title: (rowData.nickname === '') ? rowData.accountName : rowData.nickname,
+      icon: icons[rowData.accountType],
+      iconcolor: iconcolor[rowData.accountType],
+      acctnum: rowData.accountID,
+      total: rowData.accountBalance,
+      totalcolor: (rowData.accountBalance > 0) ? Skin.colors.POSITIVE_ACCENT : Skin.colors.SECONDARY_TEXT,
+    };
+    // <Text style={{color: iconcolor[rowData.accountType]}}>{icons[rowData.accountType]}</Text>
+    return cleanData;
+  }
+
+  /**
+   * Splits the account data blob into sections and data rows
+   * @param  {object} accounts object with accounts information
+   * @return {object}          Object with both the data and the sectionIds
+   */
+  renderListViewData(accounts) {
+    const data = { };
+    const sectionIds = [];
+    accounts.map((account) => {
+      const section = account.accountType;
+      if (!data[section]) {
+        data[section] = [];
+        sectionIds.push(section);
+      }
+      data[section].push(account);
+      return account;
+    });
+    return { data,sectionIds };
+  }
+
+
+  /**
+   * Creates the section header for the various account types
+   * @param  {object} data      The data blob of rows
+   * @param  {string} sectionId The id of section being applied to this row
+   * @return {JSX}              JSX of the row
+   */
+  renderSectionHeader(data, sectionId) {
+    return (
+      <ListSectionHeader>
+        {headers[sectionId]}
+      </ListSectionHeader>
+      );
+  }
+  triggerDrawer() {
+    console.log('trigger')
+    Events.trigger('toggleDrawer')
+  }
+  renderRow(rowData) {
+    const cleanData = self.cleanRowData(rowData);
+    return (
+      <ListItem>
+        <View style={styles.rowwrap}>
+          <View style={styles.iconwrap}>
+            <Text style={[styles.icon, {
+                           color: cleanData.iconcolor
+                         }]}>
+              {cleanData.icon}
+            </Text>
+          </View>
+          <View style={styles.namewrap}>
+            <Text
+              numberOfLines={1}
+              style={styles.nametext}>
+              {cleanData.title}
+            </Text>
+            <Text
+              numberOfLines={1}
+              style={styles.numtext}>
+              {cleanData.acctnum}
+            </Text>
+          </View>
+          <View style={styles.totalwrap}>
+            <View style={{ flex: 1 }}>
+      
+            </View>
+          </View>
+        </View>
+      </ListItem>
+      );
+  }
+  
+  tabChanged(e){
+  
+    this.setState({ tabChanged: e ,activeTab:e,titleString :titleStringArray[e-1]});
+  }
+  
+  getComponent(){
+    if(this.state.tabChanged===1){
+      return(<ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        renderSectionHeader={this.renderSectionHeader} />)
+    }else if(this.state.tabChanged===2){
+      return(<PayBillsScene/>)
+    }else if(this.state.tabChanged===3){
+      return(<DepositsScene/>)
+    }
+    else if(this.state.tabChanged===4){
+      return(<FindBranchScene/>)
+    }
+    else{
+      return(<ContactScene/>)
+    }
+  }
+
+  render() {
+    
+    return (
+     <Main
+        controlPanel={ControlPanel}
+        navigator={this.props.navigation}
+        defaultNav={false}
+         bottomMenu={{
+          visible: true,
+          active: this.state.tabChanged,
+        }}>
+            
+
+<View style={{ height: Platform.OS === 'android' ? 87 : 107 }}>
+        <NavBar
+          tintColor={'#fff'}
+          statusBarTint={Skin.STATUS_BAR_TINT_COLOUR}
+          statusBarLight={'light-content'}
+          titleStyle={Skin.navbar.title.titleStyle}
+          title={''}
+          titleTint={Config.LOGO_COLOR}
+          right={''}
+          left={{
+            text:this.props.isBackBtnText?'Back':'',
+            icon: this.props.isBackBtnText?'':Skin.icon.hamburger,
+            iconStyle:this.props.isBackBtnText?'': {
+              fontSize: 50,
+              paddingLeft: 17,
+              width: 50,
+              color: Config.BUTTON_BG_COLOR,
+            },
+            handler: this.props.handler
+          }} />
+
+         <View style={{ width:300,height:50, position: 'absolute', left: 60,top:30 }}> 
+        <Text style={{fontSize: 18,color:'black',backgroundColor:'transparent'}}
+          >
+          {"Your Store: Chatham,"}
+        </Text>
+        <Text style={{fontSize: 13,color:'black',backgroundColor:'transparent'}}
+          >
+          {"Open now until 9 PM"}
+        </Text>
+        </View> 
+        {/* <SearchBar
+  onChangeText={''}
+  onClearText={''}
+  placeholder='Type Here...' /> */}
+
+        <View style={{ width:50,height:50, position: 'absolute', right: 10 }}>
+           <TouchableOpacity underlayColor='transparent' onPress={this.onPressNotificationView}>
+          <Text style={{
+            color: Skin.color.BUTTON_BG_COLOR,
+            textAlign: 'center',
+            fontSize: 35,
+            height:50,
+            fontWeight: 'normal',
+            top: Platform.OS === 'android' ? 12 : 32.5,
+            backgroundColor: 'transparent',
+            fontFamily: Skin.font.ICON_FONT
+          }}>{Skin.icon.bell}</Text>
+          
+          {this.state.badgeValue > 0 && <Badge style={{
+            top: Platform.OS === 'android' ? -45 : -24,
+            right: -25,
+          }} minWidth={12} minHeight={12} extraPaddingHorizontal={2} textStyle={{ color: '#fff', }} >
+            {this.state.badgeValue>9?"9+":this.state.badgeValue}
+
+          </Badge>}
+</TouchableOpacity>
+
+        </View>
+ 
+
+      </View>
+
+
+        
+               {/* <PageTitle title={this.state.titleString}
+                handler={this.triggerDrawer}/> */}
+    
+        <View style={{
+                       flex: 1,
+                       backgroundColor: Skin.main.BACKGROUND_COLOR
+                     }}>
+
+      
+      {this.getComponent()}
+        </View>
+      </Main>
+      );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  rowwrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    fontFamily: Skin.font.ICON_FONT,
+    fontSize: 20,
+  },
+  iconwrap: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+  },
+  namewrap: {
+    flex: 3,
+  },
+  nametext: {
+    fontSize: 14,
+    color: Skin.colors.PRIMARY_TEXT,
+    paddingTop: 3,
+  },
+  numtext: {
+    fontSize: 12,
+    color: ('rgba(' + Skin.colors.PRIMARY_TEXT_RGB + ',0.54)'),
+    paddingTop: 3,
+  },
+  totalwrap: {
+    flex: 2,
+    flexDirection: 'column',
+  },
+  totaltext: {
+    fontSize: 18,
+    textAlign: 'right',
+    flex: 1,
+  },
+});
---
+++ REL-IDmobile/ReactReferenceApp/App/Components/Dashboard/ChathamRetail/PayBills.js
@@ -0,0 +1,268 @@
+'use strict';
+/*
+    NEED
+ */
+
+import React, { Component, PropTypes } from 'react';
+import ReactNative, { View, Text, StyleSheet,ListView, Image } from 'react-native'
+
+import Skin from '../../../Skin';
+import Main from '../../Container/Main';
+import ListItem from '../../ListItem';
+
+import ControlPanel from '../ControlPanel';
+import NavBar from '../../view/navbar.js';
+import PageTitle from '../../view/pagetitle.js';
+import Events from 'react-native-simple-events';
+
+import ListSectionHeader from '../../ListSectionHeader';
+
+import { FormattedCurrency } from 'react-native-globalize';
+
+
+import Config from 'react-native-config';
+
+
+
+/*
+  Instantiaions
+*/
+// const ReactRdna = React.NativeModules.ReactRdnaModule;
+const RDNARequestUtility = ReactNative.NativeModules.RDNARequestUtility;
+
+
+let self;
+const headers = {
+  1: 'Upcoming Bills',
+  2: 'Recent Payments',
+};
+const icons = {
+  1: '\ue08c',
+  2: '\ue0c1',
+};
+const iconcolor = {
+  1: Skin.colors.DIVIDER_COLOR,
+  2: Skin.colors.POSITIVE_ACCENT,
+};
+
+class PayBillsScene extends Component {
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
+    this.state = {
+      dataSource: ds.cloneWithRowsAndSections([]),
+    };
+    self = this;
+  }
+
+    /**
+     * Creates the self object for reference later
+     * @return {null}
+     */
+  componentDidMount() {
+    this.getAccountDetails();
+  }
+
+  /**
+   * Retrieves the account data in a json format
+   * @return {[type]} [description]
+   */
+  getAccountDetails() {
+//    RDNARequestUtility.doHTTPGetRequest('http://apisdkdev.uniken.com:8080/APIBanking/getAccounts.jsp?userid=10',
+//      (response) => {
+        /*
+            REAL DATA
+         "{
+           "accountList":[
+            {"accountID":44,"accountName":"CANDEMOACT10_01","accountBalance":5565,"accountType":1},
+            {"accountID":45,"accountName":"CANDEMOACT10_02","accountBalance":24322,"accountType":1},
+            {"accountID":46,"accountName":"CANDEMOACT10_03","accountBalance":30039,"accountType":1},
+            {"accountID":47,"accountName":"CANDEMOACT10_04","accountBalance":40074,"accountType":1}
+           ],
+           "error":"",
+           "status":"success"
+          }
+        */
+      var response = {
+          0: {
+            error: 0,
+            response:
+              '{"accountList":[ \
+                  {"accountID":"$16,851","accountName":"CANDEMOACT10_01","nickname":"Platinum Card","accountBalance":-165.32,"accountType":1}, \
+                  {"accountID":"$1,803","accountName":"CANDEMOACT10_03","nickname":"HSBC Interest Payment","accountBalance":-39.00,"accountType":1}, \
+                  {"accountID":" $1,203","accountName":"CANDEMOACT10_04","nickname":"Amazon Cloud Services","accountBalance":-474.52,"accountType":1}, \
+                  {"accountID":"$9,357","accountName":"CANDEMOACT10_02","nickname":"Gold Card","accountBalance":-43.22,"accountType":1}, \
+                  {"accountID":"$15,931","accountName":"CANDEMOACT10_03","nickname":"Platinum Card","accountBalance":3039.00,"accountType":2}, \
+                  {"accountID":"$9,681","accountName":"CANDEMOACT10_04","nickname":"Gold Card","accountBalance":174.52,"accountType":2}], \
+              "error":"", \
+              "status":"success" \
+            }',
+          },
+        };
+        if (response[0].error === 0) {
+          const responseJson = JSON.parse(response[0].response);
+          if (responseJson.status === 'success') {
+            const accts = responseJson.accountList;
+            const { data, sectionIds } = self.renderListViewData(accts.sort(self.compare));
+            self.setState({
+              dataSource: self.state.dataSource.cloneWithRowsAndSections(data, sectionIds),
+            });
+            return { data };
+          } else {
+            alert(responseJson.status);
+          }
+        } else {
+          alert(response[0].response);
+        }
+      //}
+    //);
+  }
+
+    /**
+     * Basic sorting function for map calls
+     */
+  compare(a, b) {
+    if (a.accountName < b.accountName) {
+      return -1;
+    }
+    if (a.accountName > b.accountName) {
+      return 1;
+    }
+    return 0;
+  }
+
+  cleanRowData(rowData) {
+    const cleanData = {
+      title: (rowData.nickname === '') ? rowData.accountName : rowData.nickname,
+      icon: icons[rowData.accountType],
+      iconcolor: iconcolor[rowData.accountType],
+      acctnum: rowData.accountID,
+      total: rowData.accountBalance,
+      totalcolor: (rowData.accountBalance > 0) ? Skin.colors.POSITIVE_ACCENT : Skin.colors.SECONDARY_TEXT,
+    };
+        // <Text style={{color: iconcolor[rowData.accountType]}}>{icons[rowData.accountType]}</Text>
+    return cleanData;
+  }
+
+    /**
+     * Splits the account data blob into sections and data rows
+     * @param  {object} accounts object with accounts information
+     * @return {object}          Object with both the data and the sectionIds
+     */
+  renderListViewData(accounts) {
+    const data = {};
+    const sectionIds = [];
+    accounts.map((account) => {
+      const section = account.accountType;
+      if (!data[section]) {
+        data[section] = [];
+        sectionIds.push(section);
+      }
+      data[section].push(account);
+      return account;
+    });
+    return { data, sectionIds };
+  }
+
+
+    /**
+     * Creates the section header for the various account types
+     * @param  {object} data      The data blob of rows
+     * @param  {string} sectionId The id of section being applied to this row
+     * @return {JSX}              JSX of the row
+     */
+  renderSectionHeader(data, sectionId) {
+    return (
+      <ListSectionHeader>{headers[sectionId]}</ListSectionHeader>
+    );
+  }
+  
+  triggerDrawer() {
+    console.log('trigger')
+    Events.trigger('toggleDrawer')
+  }
+
+  renderRow(rowData) {
+    const cleanData = self.cleanRowData(rowData);
+    return (
+      <ListItem>
+        <View style={styles.rowwrap}>
+          <View style={styles.iconwrap}>
+            <Text style={[styles.icon, { color: cleanData.iconcolor }]}>{cleanData.icon}</Text>
+          </View>
+          <View style={styles.namewrap}>
+            <Text numberOfLines={1} style={styles.nametext}>{cleanData.title}</Text>
+            <Text numberOfLines={1} style={styles.numtext}>{cleanData.acctnum}</Text>
+          </View>
+          <View style={styles.totalwrap}>
+            <View style={{ flex: 1 }}>
+             
+            </View>
+          </View>
+        </View>
+      </ListItem>
+    );
+  }
+
+  render() {
+    return (
+      
+        <View style={{ flex: 1, backgroundColor:Skin.main.BACKGROUND_COLOR }}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+            renderSectionHeader={this.renderSectionHeader}
+          />
+        </View>
+     
+    );
+  }
+}
+
+module.exports = PayBillsScene;
+
+const styles = StyleSheet.create({
+  rowwrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    fontFamily: Skin.font.ICON_FONT,
+    fontSize: 20,
+  },
+  iconwrap: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+  },
+  namewrap: {
+    flex: 3,
+        // backgroundColor:'red'
+  },
+  nametext: {
+    fontSize: 14,
+    color: Skin.colors.PRIMARY_TEXT,
+    paddingTop: 3,
+  },
+  numtext: {
+    fontSize: 12,
+    color: ('rgba(' + Skin.colors.PRIMARY_TEXT_RGB + ',0.54)'),
+    paddingTop: 3,
+  },
+  totalwrap: {
+    flex: 2,
+    flexDirection: 'column',
+        // backgroundColor: 'red'
+  },
+  totaltext: {
+    fontSize: 18,
+    textAlign: 'right',
+    flex: 1,
+        //backgroundColor:'lightblue'
+  },
+});
